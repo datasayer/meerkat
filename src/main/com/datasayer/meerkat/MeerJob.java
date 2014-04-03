fd97f39fddc9e5a40db7400842e9405ff9410987
garuda <garuda.kang@gmail.com>
2014-04-03 14:05:08 +0900

    .

--- src/main/com/datasayer/meerkat/MeerJob.java
+++ src/main/com/datasayer/meerkat/MeerJob.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import org.apache.hama.HamaConfiguration;
 import org.apache.hama.bsp.BSPJob;
 
+
 public class MeerJob extends BSPJob {
 
   public MeerJob(HamaConfiguration conf) throws IOException {
     super(conf);
+    this.setBspClass(MeerRunner.class);
   }
 
 }
