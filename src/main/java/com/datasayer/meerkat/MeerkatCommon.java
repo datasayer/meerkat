da4ca6b34dc42972aaa8cdb746366e578897cc0a
garuda <garuda.kang@gmail.com>
2014-04-07 19:07:31 +0900

    .

---
+++ src/main/java/com/datasayer/meerkat/MeerkatCommon.java
@@ -0,0 +1,12 @@
+package com.datasayer.meerkat;
+
+public class MeerkatCommon {
+  public final static long AGGREGATION_INTERVAL = 5000L;
+  public final static long POLLING_INTERVAL = 5000L;
+  public final static String AGGREGATION_INTERVAL_URI = "com.datasayer.meerkat.uri.interval.aggregation";
+  public final static String POLLING_INTERVAL_URI = "com.datasayer.meerkat.uri.interval.polling";
+  public final static String GUARDMEER_CLASS_URI = "com.datasayer.meerkat.uri.class.guardmeer";
+  public final static String BOSSMEER_CLASS_URI = "com.datasayer.meerkat.uri.class.bossmeer";
+  public final static String REPORTMEER_CLASS_URI = "com.datasayer.meerkat.uri.class.reportmeer";
+  public final static String LOG_PATH_URI = "com.datasayer.meerkat.uri.logpath";
+}
